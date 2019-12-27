34430bdfd7e5b80a034b9626037ce65c3f51fe8e
Matteo Gilardoni <m.gilardoni@mytennights.com>
2019-12-27 17:39:02 +0100

    added star logic

--- src/patternMatch/patternMatch.js
+++ src/patternMatch/patternMatch.js
@@ -1,99 +1,10 @@
-function matchQuestionMark({
-    currentChar,
-    currentPattern,
-    previousPattern
-}) {
-    let isMatching = false;
-    let isValid = false;
-    let stringIndexIncrement = 1;
-    let patternIndexIncrement = 1;
-
-    // conditions
-    let isQuestionMarkPattern = currentPattern === "?";
-    let isCharNotMatchingPreviousPattern = currentChar !== previousPattern;
-    let isCharMatchingPreviousPattern = currentChar === previousPattern;
-
-    let isZeroPreviousPattern = isQuestionMarkPattern && isCharNotMatchingPreviousPattern;
-    let isOnePreviousPattern = isQuestionMarkPattern && isCharMatchingPreviousPattern;
-
-    if (isZeroPreviousPattern) {
-        isMatching = true;
-        isValid = true;
-        stringIndexIncrement = 0;
-        patternIndexIncrement = 1;
-    }
-
-    if (isOnePreviousPattern) {
-        isMatching = true;
-        isValid = true;
-        stringIndexIncrement = 1;
-        patternIndexIncrement = 1;
-    }
-
-    return {
-        isMatching,
-        isValid,
-        stringIndexIncrement,
-        patternIndexIncrement
-    };
-}
-
-function matchDot({
-    currentChar,
-    currentPattern
-}) {
-    let isMatching = false;
-    let isValid = false;
-    let stringIndexIncrement = 1;
-    let patternIndexIncrement = 1;
-
-    // conditions
-    let isDotPattern = currentPattern === ".";
-    let isCharacter = typeof currentChar === "string" && currentChar.length === 1;
-
-    if (isDotPattern && isCharacter) {
-        isMatching = true;
-        isValid = true;
-    }
-
-    return {
-        isMatching,
-        isValid,
-        stringIndexIncrement,
-        patternIndexIncrement
-    };
-}
-
-function matchChar({
-    currentChar,
-    currentPattern
-}) {
-    let isMatching = false;
-    let isValid = false;
-    let stringIndexIncrement = 1;
-    let patternIndexIncrement = 1;
-
-    if (currentChar === currentPattern) {
-        isMatching = true;
-        isValid = true;
-    }
-
-    return {
-        isMatching,
-        isValid,
-        stringIndexIncrement,
-        patternIndexIncrement
-    };
-}
-
-function noMatch() {
-    return {
-        isMatching: true,
-        isValid: false,
-        stringIndexIncrement: 1,
-        patternIndexIncrement: 1
-    };
-}
+import {
+    matchStar,
+    matchQuestionMark,
+    matchDot,
+    matchChar,
+    noMatch
+} from "./patternChecks.js";
 
 function patternMatch(string, pattern) {
     let stringList = string.split('');
@@ -101,10 +12,12 @@ function patternMatch(string, pattern) {
     let patternList = pattern.split('');
     let patternIndex = 0;
     let validationList = [];
+    let patternMetadataList = [];
 
     function recursiveIteration() {
         let currentChar = stringList[stringIndex];
         let currentPattern = patternList[patternIndex];
+        let currentPatternMetadata = patternMetadataList[patternIndex];
         let previousPattern = patternList[patternIndex - 1];
 
         if (currentChar === undefined && currentPattern === undefined) {
@@ -112,17 +25,29 @@ function patternMatch(string, pattern) {
         }
 
         // matching engine
-        let matchers = [matchQuestionMark, matchDot, matchChar, noMatch];
+        let matchers = [
+            matchStar,
+            matchQuestionMark,
+            matchDot,
+            matchChar,
+            noMatch
+        ];
 
         for (let i = 0; i < matchers.length; i++) {
             let output = matchers[i]({
                 currentChar,
                 currentPattern,
+                currentPatternMetadata,
                 previousPattern
             });
 
             if (output.isMatching) {
-                validationList.push(output.isValid);
+                patternMetadataList[patternIndex] = output.patternMetadata;
+
+                if (output.patternIndexIncrement > 0) {
+                    validationList.push(output.isValid);
+                }
+
                 stringIndex += output.stringIndexIncrement;
                 patternIndex += output.patternIndexIncrement;
                 break;
@@ -134,9 +59,9 @@ function patternMatch(string, pattern) {
 
     recursiveIteration();
 
-    return validationList.every((value) => value);
+    return validationList.every(value => value);
 }
 
 export {
     patternMatch
-}
+};
--- src/patternMatch/patternMatch.test.js
+++ src/patternMatch/patternMatch.test.js
@@ -1,30 +1,43 @@
 import {
     patternMatch
-} from "./patternMatch.js";
+} from './patternMatch.js';
 
-describe("patternMatch", () => {
-    test("test text match", () => {
-        expect(patternMatch("a", "a")).toBe(true);
-        expect(patternMatch("aa", "a")).toBe(false);
-        expect(patternMatch("ab", "ab")).toBe(true);
-    })
+describe('patternMatch', () => {
+    test('should match when pattern character is the same character in string', () => {
+        expect(patternMatch('a', 'a')).toBe(true);
+        expect(patternMatch('aa', 'a')).toBe(false);
+        expect(patternMatch('ab', 'ab')).toBe(true);
+    });
 
-    test("test '.' match", () => {
-        expect(patternMatch("a", ".")).toBe(true);
-        expect(patternMatch("aa", ".")).toBe(false);
-        expect(patternMatch("ab", "a.")).toBe(true);
-        expect(patternMatch("abc", "a..")).toBe(true);
-        expect(patternMatch("abcd", "a..")).toBe(false);
-    })
+    test("should match when pattern is '.' and string has any character", () => {
+        expect(patternMatch('a', '.')).toBe(true);
+        expect(patternMatch('aa', '.')).toBe(false);
+        expect(patternMatch('ab', 'a.')).toBe(true);
+        expect(patternMatch('abc', 'a..')).toBe(true);
+        expect(patternMatch('abcd', 'a..')).toBe(false);
+    });
 
-    test("test '?' match", () => {
-        expect(patternMatch("", "a?")).toBe(false)
-        expect(patternMatch("a", "a?")).toBe(true)
-        expect(patternMatch("aa", "a?")).toBe(true)
-        expect(patternMatch("ab", "a?")).toBe(false)
-        expect(patternMatch("aaa", "a?")).toBe(false)
-        expect(patternMatch("ab", "a?b?")).toBe(true)
-        expect(patternMatch("aabb", "a?b?")).toBe(true)
-        expect(patternMatch("aabbc", "a?b?")).toBe(false)
-    })
-})
+    test("should match when pattern is '?' and string has 0 or 1 of the previous character", () => {
+        expect(patternMatch('', 'a?')).toBe(false);
+        expect(patternMatch('a', 'a?')).toBe(true);
+        expect(patternMatch('aa', 'a?')).toBe(true);
+        expect(patternMatch('ab', 'a?')).toBe(false);
+        expect(patternMatch('aaa', 'a?')).toBe(false);
+        expect(patternMatch('ab', 'a?b?')).toBe(true);
+        expect(patternMatch('aabb', 'a?b?')).toBe(true);
+        expect(patternMatch('aabbc', 'a?b?')).toBe(false);
+    });
+
+    test("should match when pattern is '*' and string has 1 or more of the previous character", () => {
+        expect(patternMatch('', 'a*')).toBe(false);
+        expect(patternMatch('a', 'a*')).toBe(false);
+        expect(patternMatch('aa', 'a*')).toBe(true);
+        expect(patternMatch('ab', 'a*')).toBe(false);
+        expect(patternMatch('aaa', 'a*')).toBe(true);
+        expect(patternMatch('aaab', 'a*')).toBe(false);
+        expect(patternMatch('aaab', 'a*b')).toBe(true);
+        expect(patternMatch('ab', 'a*b*')).toBe(false);
+        expect(patternMatch('aaabbb', 'a*b*')).toBe(true);
+        expect(patternMatch('aabbc', 'a*b*')).toBe(false);
+    });
+});
